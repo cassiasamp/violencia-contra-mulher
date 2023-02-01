ca92b0ed5bb6fffe1e67956fe057277e23571068
andrelamor <amorim.andrel@gmail.com>
2023-02-01 17:14:06 -0300

    inclui dado e schema inicial (inválido)

--- scripts/convert_csv.py
+++ scripts/convert_csv.py
@@ -4,11 +4,11 @@ import pandas as pd
 def convert_csv():
   # referências: https://towardsdatascience.com/how-to-export-pandas-dataframe-to-csv-2038e43d9c03
   # referências: https://colab.research.google.com/drive/1R6SHFugbCEuy5ppjDFymquj3jjbgY7Sx?authuser=1
-  xlsx_file_path = "upload/Modelo_Sesp_Feminicidio.Jan20_Dez22.xlsx"
+  xlsx_file_path = "upload/Modelo_Sesp_Feminicdio.Jan20_Dez22.xlsx"
   abas = ["Dados"]
   for aba in abas:
     read_file = pd.read_excel (xlsx_file_path, aba)
-    read_file.to_csv (f'data/{aba}.csv', index = None, header=True, sep = ';', decimal = ',', encoding = 'utf-8-sig', na_rep = "")
+    read_file.to_csv (f'data/feminicidio-2020-2022.csv', index = None, header=True, sep = ';', decimal = ',', encoding = 'utf-8-sig', na_rep = "")
 
 if __name__ == '__main__':
   convert_csv()
