4c57bd09559f5970892d7a9ced7579731040bbd4
andrelamor <amorim.andrel@gmail.com>
2023-02-01 11:44:15 -0300

    add script, make e config.mk

---
+++ scripts/convert_csv.py
@@ -0,0 +1,16 @@
+import sys
+import pandas as pd
+
+def convert_csv():
+  # referências: https://towardsdatascience.com/how-to-export-pandas-dataframe-to-csv-2038e43d9c03
+  # referências: https://colab.research.google.com/drive/1R6SHFugbCEuy5ppjDFymquj3jjbgY7Sx?authuser=1
+  xlsx_file_path = "upload/Modelo_Sesp_Feminicidio.Jan20_Dez22.xlsx"
+  abas = ["Dados"]
+  for aba in abas:
+    read_file = pd.read_excel (xlsx_file_path, aba)
+    read_file.to_csv (f'data/{aba}.csv', index = None, header=True, sep = ';', decimal = ',', encoding = 'utf-8-sig', na_rep = "")
+
+if __name__ == '__main__':
+  convert_csv()
+
+# AttributeError: module 'sys' has no attribute 'arg'
