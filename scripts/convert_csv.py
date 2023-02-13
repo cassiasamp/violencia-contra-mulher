5788d8d8816b59cb205934873e881f508a32336b
andrelamor <amorim.andrel@gmail.com>
2023-02-13 16:43:30 -0300

    corrige conversão de nome de coluna em script

--- scripts/convert_csv.py
+++ scripts/convert_csv.py
@@ -15,7 +15,7 @@ def convert_csv():
     feminicidio = pd.read_excel('http://www.seguranca.mg.gov.br/images/2023/Janeiro/Modelo_Sesp_Feminicdio.Jan20_Dez22.xlsx', sheet_name='Dados', names=feminicidio.columns, header=0)
     feminicidio.to_csv (f'data/feminicidio-2020-2022.csv', index = None, header=True, sep = ';', decimal = ',', encoding = 'utf-8-sig', na_rep = "")
     violencia = pd.read_excel('http://www.seguranca.mg.gov.br/images/2023/Janeiro/Modelo_Sesp_Violencia%20Domestica%20e%20Familiar%20contra%20a%20Mulher.Jan20_Dez22.xlsx', sheet_name='Dados')
-    violencia.rename(columns = {'MUNICÍPIO-CÓD':'MUNICIPIO_COD','MUNICÍPIO':'MUNICIPIO','DATA DO FATO':'DATA_FATO','MÊS':'MES','QUANT. VÍTIMAS VIOL.DOMÉSTICA':'QUANT_VITIMAS_VIOL.DOMESTICA'}, inplace=True)
+    violencia.rename(columns = {'MUNICÍPIO-CÓD':'MUNICIPIO_COD','MUNICÍPIO':'MUNICIPIO','DATA FATO':'DATA_FATO','MÊS':'MES','QUANT. VÍTIMAS VIOL.DOMÉSTICA':'QUANT_VITIMAS_VIOL.DOMESTICA'}, inplace=True)
     violencia = pd.read_excel('http://www.seguranca.mg.gov.br/images/2023/Janeiro/Modelo_Sesp_Violencia%20Domestica%20e%20Familiar%20contra%20a%20Mulher.Jan20_Dez22.xlsx', sheet_name='Dados', names=violencia.columns, header=0)
     violencia.to_csv (f'data/violencia-contra-mulher-2020-2022.csv', index = None, header=True, sep = ';', decimal = ',', encoding = 'utf-8-sig', na_rep = "")
 if __name__ == '__main__':
